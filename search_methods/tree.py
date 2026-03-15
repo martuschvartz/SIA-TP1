a8ed2304043be88d3efe7e1c72ca5829393f1317
juane993 <jolivamorroni@itba.edu.ar>
2026-03-15 19:28:48 -0300

    feat: modified tree structure and node
    Co-authored-by: Juan Oliva <jolivamorroni@itba.edu.ar>
    Co-authored-by: Fernando Li <feli@itba.edu.ar>

--- search_methods/tree.py
+++ search_methods/tree.py
@@ -2,16 +2,26 @@ from search_methods.node import TreeNode
 from typing import List
 from queue import Queue
 
+from sokoban_engine import BoardState, Board
+
+
 # Searching Tree
 
 class Tree:
-    def __init__(self, root: TreeNode):
+    #dfs
+    #bfs
+    #def __init__(self, root: TreeNode, func(BoardState , int ) orderMethod:
+    def __init__(self, board: Board, init_state: BoardState,  ):
         #nodo raiz
-        self.root = root
+        self.root = TreeNode(init_state, board, 0, False)
+
         # lista de nodos frontera
         self.frontLineNodes:Queue = Queue(0)
         # lista de nodos explorados. TODO: maybe esto puede ser un Set
         self.exploredNodes : List[TreeNode] = []
+
+    def calculate_heuristic(self, state:BoardState) -> int:
+        return self.root.search_method.calculate_heuristic(state)
 
 
     def start_searching(self):
@@ -22,11 +32,14 @@ class Tree:
             if node.get_is_goal:
                 return None
             self.exploredNodes.append(node)
-            ListNode = node.expand()
+            # TODO: ver si sortear el orden dependiendo del metodo
+            ListNode = node.expand().sortBy(orderMethod)
+
             for child in ListNode:
-                # TODO: ver si sortear el orden dependiendo del metodo
                 # todos los nodos ya tienen sus hijos guardados.
                 self.frontLineNodes.put(child)
+
+
 
 
 
